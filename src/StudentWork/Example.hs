e86c9dd901c18bb5baa3859cca90e41c047bdd5c
Mikhail Maliugin <ghastmisha089@gmail.com>
2024-11-09 15:12:19 +0200

    deleted from example

--- src/StudentWork/Example.hs
+++ src/StudentWork/Example.hs
@@ -1,17 +1,1 @@
 module StudentWork.Example where
-
-data Exp a where
-    EConst :: Int -> Exp a
-    EVar :: a -> Exp a
-    ESum :: Exp a -> Exp a -> Exp a
-    EProd :: Exp a -> Exp a -> Exp a    
-
-mapExp :: (a -> b) -> Exp a -> Exp b
-mapExp _ (EConst x) = EConst x
-mapExp f (EVar x)   = EVar (f x) 
-mapExp f (ESum x y) = ESum (mapExp f x) (mapExp f y)
-mapExp f (EProd x y) = EProd (mapExp f x) (mapExp f y)
-
-foldrExp :: (Exp a -> Exp b -> Exp b) -> Exp b -> [Exp a] -> Exp b
-foldrExp _ x [] = x
-foldrExp f x (y:ys) = f y (foldrExp f x ys)
